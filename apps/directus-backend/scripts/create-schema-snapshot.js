d4fd7db068b66f15f47f1e2c356b8db418eeabda
Blechlawine <marczinser@gmx.de>
2022-11-14 11:16:10 +0100

    start database model

---
+++ apps/directus-backend/scripts/create-schema-snapshot.js
@@ -0,0 +1,9 @@
+const exec = require("child_process").exec;
+
+let now = new Date();
+let dateStr = `${now.getUTCFullYear()}-${now.getUTCMonth()}-${now.getUTCDate()}__${now.getUTCHours()}-${now.getUTCMinutes()}-${now.getUTCSeconds()}`;
+let cmd = `yarn directus schema snapshot --yes ./schema/schema.yml`;
+
+exec(cmd, (error, stdout, stderr) => {
+    console.log(stdout);
+});
