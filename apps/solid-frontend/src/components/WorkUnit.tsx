d9c3b50e9c7b0c49c081cdf7ab404394e6c2c420
Blechlawine <marczinser@gmx.de>
2022-11-21 14:10:59 +0100

    new layout for workunits

--- apps/solid-frontend/src/components/WorkUnit.tsx
+++ apps/solid-frontend/src/components/WorkUnit.tsx
@@ -15,14 +15,23 @@ const WorkUnit: Component<TProps> = (props) => {
         () => servers.currentServer(),
         async () => await servers.currentServer()?.project.getById(unit.project)
     );
+    const [category, categoryResource] = createResource(
+        () => servers.currentServer(),
+        async () => await servers.currentServer()?.category.getById(unit.category)
+    );
     return (
-        <div class="p-2 bg-base-200 rounded-lg">
-            <div class="time">
+        <div class="grid grid-cols-[max-content_1fr_1fr] p-2 gap-2 bg-base-200 rounded-lg">
+            <div class="flex flex-col gap-2">
                 <span>{dayjs(unit?.start).format("HH:mm")}</span>
-                {" - "}
                 <span>{dayjs(unit?.end).format("HH:mm")}</span>
             </div>
-            <span>{project()?.title}</span>
+            <div
+                class="flex flex-col gap-2 pl-2 border-solid border-base-300"
+                classList={{ "border-l-2": !unit?.description, "border-x-2": !!unit?.description }}
+            >
+                <span>{project()?.title}</span>
+                <span>{category()?.name}</span>
+            </div>
             <p>{unit?.description}</p>
         </div>
     );
