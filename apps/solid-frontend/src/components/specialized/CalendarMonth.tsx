e6329ad5d2f89958eed46dfc9f345580489a2a53
Blechlawine <marczinser@gmx.de>
2023-01-02 15:21:13 +0100

    seperate daygrid

--- apps/solid-frontend/src/components/specialized/CalendarMonth.tsx
+++ apps/solid-frontend/src/components/specialized/CalendarMonth.tsx
@@ -1,6 +1,6 @@
 import dayjs from "dayjs";
 import { ChevronLeft, ChevronRight } from "lucide-solid";
-import { Accessor, Component, createMemo } from "solid-js";
+import { Accessor, Component, ComponentProps, createMemo, For, JSX } from "solid-js";
 import { Dynamic } from "solid-js/web";
 import Button from "~/Button";
 import ForNumber from "~/ForNumber";
@@ -20,10 +20,9 @@ type TProps = {
 
 const CalendarMonth: Component<TProps> = (props) => {
     const { now, onUpdateNow, controls = true } = props;
-    const weekAmount = createMemo(() => now().date(now().daysInMonth()).day(0).diff(now().date(1).day(0), "week") + 1);
 
     return (
-        <div class="h-full grid grid-rows-[min-content_min-content_1fr] p-1">
+        <div class="h-full grid grid-rows-[min-content_1fr] p-1">
             <div
                 class="flex flex-row items-center gap-2 w-full"
                 classList={{
@@ -48,33 +47,18 @@ const CalendarMonth: Component<TProps> = (props) => {
                     />
                 )}
             </div>
-            <div class="grid grid-cols-7">
-                {/* Weekday names above the columns */}
-                <ForNumber each={7}>
-                    {(day) => (
-                        <div class="flex flex-row items-center justify-center p-2">
-                            {dayjs().weekday(day).format("ddd")}
-                        </div>
-                    )}
-                </ForNumber>
-            </div>
-            <div
-                class="grid grid-cols-7 gap-1 h-full"
-                classList={{
-                    "grid-rows-5": weekAmount() === 5,
-                    "grid-rows-6": weekAmount() === 6,
-                }}
-            >
-                {/* Fill the days before the first of this month with the days from the previous month */}
-                <ForNumber each={now().date(1).weekday()}>
-                    {(day) => (
-                        <div class="opacity-50">
-                            {now().subtract(1, "month").daysInMonth() + (day - now().date(0).weekday())}
-                        </div>
-                    )}
-                </ForNumber>
-                {/* Days of this month */}
-                <ForNumber each={now().daysInMonth()}>
+            <div class="relative h-full grid grid-rows-[min-content_1fr]">
+                <div class="grid grid-cols-7">
+                    {/* Weekday names above the columns */}
+                    <ForNumber each={7}>
+                        {(day) => (
+                            <div class="flex flex-row items-center justify-center p-2">
+                                {dayjs().weekday(day).format("ddd")}
+                            </div>
+                        )}
+                    </ForNumber>
+                </div>
+                <Grid now={now}>
                     {(day) => (
                         <div class="border-base-100 border-2 rounded-lg px-2 py-1">
                             {day + 1}
@@ -90,13 +74,45 @@ const CalendarMonth: Component<TProps> = (props) => {
                                 ))}
                         </div>
                     )}
-                </ForNumber>
-                {/* Fill the rest of the days with days from the next month */}
-                <ForNumber each={6 - now().date(now().daysInMonth()).weekday()}>
-                    {(day) => <div class="opacity-50">{day + 1}</div>}
-                </ForNumber>
+                </Grid>
             </div>
         </div>
     );
 };
+
+type TGridProps = Omit<ComponentProps<typeof For<number, JSX.Element>>, "each"> & {
+    now: Accessor<dayjs.Dayjs>;
+};
+
+const Grid: Component<TGridProps> = (props) => {
+    const { now } = props;
+
+    const weekAmount = createMemo(() => now().date(now().daysInMonth()).day(0).diff(now().date(1).day(0), "week") + 1);
+
+    return (
+        <div
+            class="grid grid-cols-7 gap-1 h-full w-full"
+            classList={{
+                "grid-rows-5": weekAmount() === 5,
+                "grid-rows-6": weekAmount() === 6,
+            }}
+        >
+            {/* Fill the days before the first of this month with the days from the previous month */}
+            <ForNumber each={now().date(1).weekday()}>
+                {(day) => (
+                    <div class="opacity-50">
+                        {now().subtract(1, "month").daysInMonth() + (day - now().date(0).weekday())}
+                    </div>
+                )}
+            </ForNumber>
+            {/* Days of this month */}
+            <ForNumber each={now().daysInMonth()}>{props.children}</ForNumber>
+            {/* Fill the rest of the days with days from the next month */}
+            <ForNumber each={6 - now().date(now().daysInMonth()).weekday()}>
+                {(day) => <div class="opacity-50">{day + 1}</div>}
+            </ForNumber>
+        </div>
+    );
+};
+
 export default CalendarMonth;
