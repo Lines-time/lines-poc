583b054681850186d3efbc3f954a665544216790
Blechlawine <marczinser@gmx.de>
2023-01-02 16:33:14 +0100

    auto alignment and auto location on dropdown

--- apps/solid-frontend/src/components/Dropdown.tsx
+++ apps/solid-frontend/src/components/Dropdown.tsx
@@ -1,4 +1,4 @@
-import { createMemo, JSX, ParentComponent } from "solid-js";
+import { createMemo, createSignal, JSX, onCleanup, onMount, ParentComponent } from "solid-js";
 
 type TProps = {
     label: JSX.Element;
@@ -7,36 +7,80 @@ type TProps = {
     hover?: boolean;
     labelClass?: string;
     class?: string;
+    autoLocation?: boolean;
+    autoAlignment?: boolean;
 };
 
 const Dropdown: ParentComponent<TProps> = (props) => {
     const {
         label,
-        alignment = "start",
-        location = "bottom",
         hover,
         children,
         labelClass = "btn",
+        autoLocation = true,
+        autoAlignment = true,
     } = props;
+    let rootRef: HTMLDivElement | undefined;
+    let dropdownRef: HTMLDivElement | undefined;
+    const [location, setLocation] = createSignal(props.location ?? "bottom");
+    const [alignment, setAlignment] = createSignal(props.alignment ?? "start");
+
+    const updateAutoLocation = () => {
+        if (autoLocation && rootRef && dropdownRef) {
+            if (
+                window.innerHeight - rootRef.getBoundingClientRect().bottom <=
+                    dropdownRef.clientHeight &&
+                location() !== "top"
+            ) {
+                setLocation("top");
+            }
+        }
+    };
+
+    const updateAutoAlignment = () => {
+        if (autoAlignment && rootRef && dropdownRef) {
+            if (
+                window.innerWidth - rootRef.getBoundingClientRect().left <=
+                    dropdownRef.clientWidth &&
+                alignment() !== "end"
+            ) {
+                setAlignment("end");
+            }
+        }
+    };
+
+    onMount(() => {
+        updateAutoLocation();
+        updateAutoAlignment();
+        window.addEventListener("resize", updateAutoLocation);
+        window.addEventListener("resize", updateAutoAlignment);
+    });
+
+    onCleanup(() => {
+        window.removeEventListener("resize", updateAutoLocation);
+        window.removeEventListener("resize", updateAutoAlignment);
+    });
+
     const classes = createMemo(() => ({
-        "dropdown-end": alignment === "end",
-        "dropdown-top": location === "top",
-        "dropdown-bottom": location === "bottom",
-        "dropdown-left": location === "left",
-        "dropdown-right": location === "right",
+        "dropdown-end": alignment() === "end",
+        "dropdown-top": location() === "top",
+        "dropdown-bottom": location() === "bottom",
+        "dropdown-left": location() === "left",
+        "dropdown-right": location() === "right",
         "dropdown-hover": hover,
     }));
     return (
-        <div class={`dropdown ${props.class}`} classList={classes()}>
+        <div ref={rootRef} class={`dropdown ${props.class}`} classList={classes()}>
             <label tabindex="0" class={`${labelClass}`}>
                 {label}
             </label>
-            <ul
+            <div
+                ref={dropdownRef}
                 tabIndex={0}
                 class="dropdown-content p-2 shadow bg-base-300 rounded-xl w-64 menu menu-compact"
             >
                 {children}
-            </ul>
+            </div>
         </div>
     );
 };
