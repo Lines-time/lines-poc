65d8e943c5ca1f72be701a9a6cb19ab39c628775
Blechlawine <marczinser@gmx.de>
2023-01-04 12:25:42 +0100

    add group array function

--- apps/solid-frontend/src/utils/utils.ts
+++ apps/solid-frontend/src/utils/utils.ts
@@ -1,6 +1,12 @@
 import dayjs from "dayjs";
 
-export const scale = (value: number, maxIn: number, minIn: number, maxOut: number, minOut: number): number => {
+export const scale = (
+    value: number,
+    maxIn: number,
+    minIn: number,
+    maxOut: number,
+    minOut: number
+): number => {
     return ((value - minIn) / (maxIn - minIn)) * (maxOut - minOut) + minOut;
 };
 
@@ -12,3 +18,10 @@ export const parseTimeStringDuration = (time?: string) =>
         minutes: parseTimeString(time).minute(),
         seconds: parseTimeString(time).second(),
     });
+
+export function groupArray<T, K extends keyof T>(array: Array<T>, key: K) {
+    return array.reduce(
+        (map, item) => map.set(item[key], [...(map.get(item[key]) || []), item]),
+        new Map<T[K], T[]>()
+    );
+}
