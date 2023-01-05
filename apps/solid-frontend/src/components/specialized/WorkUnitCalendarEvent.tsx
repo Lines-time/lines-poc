a60757a43ee42ea6274f5da416c17f919b374b34
Blechlawine <marczinser@gmx.de>
2023-01-05 12:51:40 +0100

    improve workunit element in calendars

--- apps/solid-frontend/src/components/specialized/WorkUnitCalendarEvent.tsx
+++ apps/solid-frontend/src/components/specialized/WorkUnitCalendarEvent.tsx
@@ -1,14 +1,32 @@
-import type { TWorkUnit } from "lines-types";
-import type { Component } from "solid-js";
+import dayjs from "dayjs";
+import { Component, createResource } from "solid-js";
 
+import categoryStore from "../../store/categoryStore";
+import projectStore from "../../store/projectStore";
+
+import type { TWorkUnit } from "lines-types";
 type TProps = {
     workUnit: TWorkUnit;
 };
 
 const WorkUnitCalendarEvent: Component<TProps> = (props) => {
+    const [project] = createResource(
+        async () => await projectStore.getById(props.workUnit.project)
+    );
+    const [category] = createResource(
+        async () => await categoryStore.getById(props.workUnit.category)
+    );
     return (
         <div class="w-full h-full bg-primary text-primary-content border-2 rounded border-primary-focus">
-            <p>{props.workUnit.description}</p>
+            <p class="p-1 px-2">
+                {project()?.title}: {category()?.name}
+                <br />
+                {dayjs(props.workUnit.start).format("H:mm")}-
+                {dayjs(props.workUnit.end).format("H:mm")}
+            </p>
+            <hr class="border-primary-focus border-t-2" />
+            <label class="text-xs px-2">Description:</label>
+            <p class="px-2">{props.workUnit.description}</p>
         </div>
     );
 };
