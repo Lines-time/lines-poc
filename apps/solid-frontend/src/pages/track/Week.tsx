444517cbc9c18c2e3eaa230c5145209c7f1f5576
Blechlawine <marczinser@gmx.de>
2023-01-05 15:28:14 +0100

    add workunit modal to week view

--- apps/solid-frontend/src/pages/track/Week.tsx
+++ apps/solid-frontend/src/pages/track/Week.tsx
@@ -1,18 +1,26 @@
+import { useNavigate, useSearchParams } from "@solidjs/router";
 import dayjs from "dayjs";
-import { Component, createMemo, createResource, createSignal } from "solid-js";
+import { X } from "lucide-solid";
+import { Component, createMemo, createResource, createSignal, Show } from "solid-js";
+import Button from "~/Button";
+import WorkUnitForm from "~/modals/WorkUnitForm";
 import CalendarWeek from "~/specialized/Calendar/CalendarWeek";
 import WorkUnitCalendarEvent from "~/specialized/WorkUnitCalendarEvent";
 
 import settingsStore from "../../store/settingsStore";
 import workUnitStore from "../../store/workUnitStore";
 
+import type { TWorkUnit } from "lines-types";
+
 const Week: Component = () => {
+    const [searchParams, setSearchParams] = useSearchParams();
+    const navigate = useNavigate();
     const [now, setNow] = createSignal(dayjs());
 
     const [settings] = createResource(async () => await settingsStore.get());
     const trackingInterval = createMemo(() => settings()?.tracking_increment ?? 30);
 
-    const [workUnits] = createResource(
+    const [workUnits, workUnitsResource] = createResource(
         async () =>
             await workUnitStore.getForDateRangeAndUser(
                 now().isoWeekday(1).toDate(),
@@ -20,17 +28,73 @@ const Week: Component = () => {
             )
     );
 
+    const closeEdit = () => {
+        setSearchParams({ edit: undefined });
+    };
+
+    const clickWorkUnit = (workUnit: TWorkUnit) => {
+        navigate(`?edit=${workUnit.id}#${workUnit.id}`, { replace: true });
+    };
+
+    const workUnitModalPresetData = createMemo(() => {
+        if (searchParams.edit === "new") {
+            const units = workUnits();
+            if (units) {
+                const lastUnit = units[units.length - 1];
+                if (lastUnit) {
+                    return {
+                        start: lastUnit.end,
+                    };
+                }
+            }
+        } else if (searchParams.edit) {
+            const _wu = workUnits()?.find((wu) => wu && wu.id === searchParams.edit);
+            if (_wu)
+                return {
+                    id: _wu.id,
+                    start: _wu.start,
+                    end: _wu.end,
+                    project: _wu.project,
+                    category: _wu.category,
+                    description: _wu.description,
+                };
+        }
+        return {};
+    });
+
     const events = createMemo(
         () =>
             workUnits()?.map((wu) => ({
                 start: dayjs(wu.start).toDate(),
                 end: dayjs(wu.end).toDate(),
-                display: () => <WorkUnitCalendarEvent workUnit={wu} />,
+                display: () => (
+                    <WorkUnitCalendarEvent workUnit={wu} onClick={() => clickWorkUnit(wu)} />
+                ),
             })) ?? []
     );
+
     return (
-        <div class="overflow-auto">
+        <div class="overflow-auto relative">
             <CalendarWeek now={now} controls={false} interval={trackingInterval} events={events} />
+            <Show when={searchParams.edit}>
+                <div class="fixed top-20 right-8 bg-base-300 rounded-lg border-solid border-base-100 border-2 z-10 w-1/4">
+                    <div class="">
+                        <div class="flex flex-row p-2 pl-3 justify-between">
+                            <h2 class="font-bold text-xl">
+                                {searchParams.edit === "new" ? "Create new" : "Edit"}
+                            </h2>
+                            <Button class="btn-sm" icon={X} onClick={() => closeEdit()} />
+                        </div>
+                        <div class="p-2">
+                            <WorkUnitForm
+                                presetData={workUnitModalPresetData()}
+                                onClose={() => closeEdit()}
+                                onSave={() => workUnitsResource.refetch()}
+                            />
+                        </div>
+                    </div>
+                </div>
+            </Show>
         </div>
     );
 };
