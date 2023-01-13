1e48421c663531a78a64113b5c0c0c49f3c1c1c8
Blechlawine <marczinser@gmx.de>
2023-01-13 18:38:59 +0100

    setup rome linter for entire repo

--- apps/directus-backend/scripts/create-schema-snapshot.js
+++ apps/directus-backend/scripts/create-schema-snapshot.js
@@ -2,7 +2,7 @@ const exec = require("child_process").exec;
 
 let now = new Date();
 let dateStr = `${now.getUTCFullYear()}-${now.getUTCMonth()}-${now.getUTCDate()}__${now.getUTCHours()}-${now.getUTCMinutes()}-${now.getUTCSeconds()}`;
-let cmd = `yarn directus schema snapshot --yes ./schema/schema.yml`;
+let cmd = "yarn directus schema snapshot --yes ./schema/schema.yml";
 
 exec(cmd, (error, stdout, stderr) => {
     console.log(stdout);
