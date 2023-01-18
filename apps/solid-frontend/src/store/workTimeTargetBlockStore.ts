9bf96cdee147f3f831e2ca3c85d240501c074c18
Blechlawine <marczinser@gmx.de>
2023-01-18 10:47:36 +0100

    fix fetching wrong users worktime target blocks

--- apps/solid-frontend/src/store/workTimeTargetBlockStore.ts
+++ apps/solid-frontend/src/store/workTimeTargetBlockStore.ts
@@ -5,45 +5,56 @@ import { directus } from "../utils/directus";
 const [workTimeTargetBlocks] = createStore({
     get getById() {
         return async (id: string) => {
-            const result = await directus.items("WorkTimeTargetBlock").readOne(id);
+            const result = await directus
+                .items("WorkTimeTargetBlock")
+                .readOne(id);
             return result;
         };
     },
     get getForUser() {
-        return async (userId: string) => {
-            const result = await directus.items("WorkTimeTargetBlock").readByQuery({
-                filter: {
-                    workerId: {
-                        id: {
-                            _eq: userId,
+        return async (userId = "$CURRENT_USER") => {
+            const result = await directus
+                .items("WorkTimeTargetBlock")
+                .readByQuery({
+                    filter: {
+                        workerId: {
+                            id: {
+                                _eq: userId,
+                            },
                         },
                     },
-                },
-            });
+                });
             return result.data;
         };
     },
     get getCurrent() {
         return async () => {
-            const result = await directus.items("WorkTimeTargetBlock").readByQuery({
-                filter: {
-                    start: {
-                        _lte: new Date(),
+            const result = await directus
+                .items("WorkTimeTargetBlock")
+                .readByQuery({
+                    filter: {
+                        workerId: {
+                            id: {
+                                _eq: "$CURRENT_USER",
+                            },
+                        },
+                        start: {
+                            _lte: new Date(),
+                        },
+                        _or: [
+                            {
+                                end: {
+                                    _gte: new Date(),
+                                },
+                            },
+                            {
+                                end: {
+                                    _null: true,
+                                },
+                            },
+                        ],
                     },
-                    _or: [
-                        {
-                            end: {
-                                _gte: new Date(),
-                            },
-                        },
-                        {
-                            end: {
-                                _null: true,
-                            },
-                        },
-                    ],
-                },
-            });
+                });
             return result.data;
         };
     },
