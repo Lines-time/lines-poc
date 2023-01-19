561e7261c61180f747ecccbc973ecce60ad9646f
Blechlawine <marczinser@gmx.de>
2023-01-19 17:20:15 +0100

    fix background color of progress bar

--- apps/solid-frontend/src/pages/Track.tsx
+++ apps/solid-frontend/src/pages/Track.tsx
@@ -55,7 +55,7 @@ const Track: Component = () => {
                             </span>
                         </span>
                         <progress
-                            class="progress progress-primary"
+                            class="progress progress-primary bg-base-100"
                             max={100}
                             value={progress().workedPercent()}
                         />
